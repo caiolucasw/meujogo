a480e44e43198ae0f85ac5fd44d71c06841d0666
Caio Silveira <caiolsds@gmail.com>
2020-01-10 08:12:21 -0300

    Modificando o código e adicionando sprites

--- arquivo1.py
+++ arquivo1.py
@@ -1,35 +1,125 @@
 import pygame
 import sys
 
+# -------------------------------------------------------------------- IMAGES ---------------------------------------------------------------
+
+sprites_right = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'),
+                 pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'),
+                 pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
+
+sprites_left = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'),
+                pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'),  pygame.image.load('L8E.png'),
+                pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# global variables
+
+bg = pygame.image.load('bg.jpg')
+resolucao = width, height = (500,480)
+posicao_circ = [50, 400]
+COR = (0, 255, 0)
+raio = 20
+veloc = 5
+PRETO = (0,0,0)
+isJump = False
+jumpCount = 10
+tela = pygame.display.set_mode(resolucao)
+pygame.init()
+left = False
+right = False
+straight = False
+contWalk = 0
+clock = pygame.time.Clock()
+
+
+
+def draw_images():
+    global tela
+    global contWalk
+    global left
+    global right
+    global straight
+
+    tela.blit(bg, (0,0))
+
+    if contWalk + 1 == len(sprites_right):
+        contWalk = 0
+
+    if left:
+        tela.blit(sprites_left[contWalk],(posicao_circ[0], posicao_circ[1]))
+
+    elif right or straight:
+        tela.blit(sprites_right[contWalk], (posicao_circ[0], posicao_circ[1]))
+
+    else:
+        tela.blit(sprites_right[contWalk], (posicao_circ[0], posicao_circ[1]))
+
+    contWalk += 1
+
+    pygame.display.update()
+
+
 def main():
+    global isJump
+    global jumpCount
+    global tela
+    global right
+    global left
+    global straight
+    global clock
 
-    resolucao = height, width = 800, 600
-    COR = (0,255,0)
-    POSICAO_CIRCULO = (400, 400)
-    RAIO = 20
-
-    pygame.init()
-    tela = pygame.display.set_mode(resolucao)
+    clock.tick(11)
 
     while True:
 
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
+        tela
+
+        pygame.time.delay(50)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 sys.exit()
 
-            if pygame.type == pygame.K_DOWN:
-                POSICAO_CIRCULO[1] += 30
+        lista = pygame.key.get_pressed()
+
+        if lista[pygame.K_LEFT] and posicao_circ[0] - veloc >= raio:
+            posicao_circ[0] -= veloc
+            left = True
+            right = False
+            straight = False
 
 
+        if lista[pygame.K_RIGHT] and posicao_circ[0] + veloc <= width - raio:
+            posicao_circ[0] += veloc
+            right = True
+            left = False
+            straight = False
 
-            print(evento.type)
+        if lista[pygame.K_SPACE]:
+            isJump = True
+            straight = True
+            left = False
+            right = False
 
-        pygame.draw.circle(tela,COR,(POSICAO_CIRCULO[0], POSICAO_CIRCULO[1]),RAIO)
-        pygame.display.update()
+        if isJump:
+            flag = 1
+            if jumpCount < 0:
+                flag = -1
+            if jumpCount >= -10:
+                posicao_circ[1] -= round(abs(jumpCount*5)*0.5*flag)
+                jumpCount -= 1
+
+            else:
+                jumpCount = 10
+                isJump = False
+
+
+        draw_images()
+
 
 
 if __name__ == '__main__':
     main()
-
-
-
